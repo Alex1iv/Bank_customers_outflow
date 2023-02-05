9cc4bdc52ddb7935748804bce11e9afeab870bc7
Alex1iv <ivanov.politehnik@gmail.com>
2023-02-06 00:15:12 +0600

    run via streamlit application

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from utils.functions import get_data
+
+import sys
+from streamlit.web import cli as stcli
+
 
 if __name__ == "__main__":
-    print('The program returns f1 score')
     
-    get_data()
+    # get f1-score:
+    #--------------------
+    #get_data() 
+    
+    
+    # otherwise get a prediction in a web page 
+    #--------------------   
+    print('The program returns prediction by given data')
+    #start_streamlit()
+    
+    sys.argv = ["streamlit", "run", "utils/application.py"]
+    
+    sys.exit(stcli.main())
+    
+   
